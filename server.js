1c11b3e61eafe1ffed7722dcf549cc2732ac68cc
Boshra Jaber <boshrajaber94@gmail.com>
2021-02-21 14:08:41 +0200

    basic stracture

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 'use strict';
 
-
+//require statement (importing packages)
 
 let express = require('express');
+// intialiazation 
 
 let app = express();
-app.listen()
+
+require('dotenv').config;
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, ()=> {
+    console.log("it is listening");
+})
